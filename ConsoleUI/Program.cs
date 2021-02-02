00ce357fcb150ad4351882d5aa01a8ef2323bd1a
Ahmet Emre Apo <apoahmetemre@gmail.com>
2021-02-02 21:33:53 +0300

    Program.cs classında tüm metotlar bulunuyor. Deneyebilirsiniz.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+using Entities.Concrete;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            Car car1 = new Car {Id=7, BrandId=2, ColorId=1, DailyPrice=450, ModelYear=2021, Description="Son Model Araba" };
+
+            Console.WriteLine("~~.car1 eklendikten sonra.~~");
+            carManager.Add(car1);
+
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id);
+            }
+
+            
+
+        }
+    }
+}
